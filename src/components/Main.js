bef747a26a812b7ed00834a10e4c8639c0a53da4
kielx <kiel66@gmail.com>
2021-08-08 10:27:28 +0200

    Fix immediate transition to results screen on incorrect answer. Fix hover yellow background while answer is selected.

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,6 +13,7 @@ const Main = ({
   setIncorrect,
 }) => {
   const [answerOrder, setAnswerOrder] = useState([]);
+  const [clicked, setClicked] = useState(false);
 
   const checkAnswer = (event) => {
     if (!answered) {
@@ -74,7 +75,37 @@ const Main = ({
           Country Quiz
         </h1>
 
-        {!incorrect ? (
+        {incorrect && clicked ? (
+          <div className="flex flex-col items-center p-10 gap-3">
+            <img
+              aria-hidden="true"
+              src={hiScore}
+              className="w-1/2"
+              alt="A around the globe traveller"
+            ></img>
+            <h2 className="QuestionText text-3xl text-indigo-800 text-center font-extrabold pt-8  uppercase">
+              Results
+            </h2>
+            <p>
+              You got{" "}
+              <span className="text-3xl text-green-400 font-bold">
+                {points}
+              </span>{" "}
+              correct answers!
+            </p>
+            <button
+              className="mt-10 w-1/2 border-2 border-indigo-800 p-3 text-indigo-800 rounded-xl cursor-pointer transition-all hover:bg-yellow-500 hover:border-yellow-500 hover:text-white"
+              onClick={() => {
+                setIncorrect(false);
+                setAnswered(false);
+                setPoints(0);
+                setClicked(false);
+              }}
+            >
+              Try Again
+            </button>
+          </div>
+        ) : (
           <>
             <img
               aria-hidden="true"
@@ -100,41 +131,15 @@ const Main = ({
                 className={`${
                   answered ? "visible" : "invisible"
                 } btn-selected `}
-                onClick={() => setAnswered(false)}
+                onClick={() => {
+                  setAnswered(false);
+                  incorrect ? setClicked(true) : setClicked(false);
+                }}
               >
                 NEXT
               </button>
             </div>
           </>
-        ) : (
-          <div className="flex flex-col items-center p-10 gap-3">
-            <img
-              aria-hidden="true"
-              src={hiScore}
-              className="w-1/2"
-              alt="A around the globe traveller"
-            ></img>
-            <h2 className="QuestionText text-3xl text-indigo-800 text-center font-extrabold pt-8  uppercase">
-              Results
-            </h2>
-            <p>
-              You got{" "}
-              <span className="text-3xl text-green-400 font-bold">
-                {points}
-              </span>{" "}
-              correct answers!
-            </p>
-            <button
-              className="mt-10 w-1/2 border-2 border-indigo-800 p-3 text-indigo-800 rounded-xl cursor-pointer transition-all hover:bg-yellow-500 hover:border-yellow-500 hover:text-white"
-              onClick={() => {
-                setIncorrect(false);
-                setAnswered(false);
-                setPoints(0);
-              }}
-            >
-              Try Again
-            </button>
-          </div>
         )}
       </div>
     </main>
