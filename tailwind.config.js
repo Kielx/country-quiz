24b66c96928a527dfb34b642cba0ed1441a27933
kielx <kiel66@gmail.com>
2021-08-08 10:53:28 +0200

    Add tailwind safelist for ordering of items

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    options: {
+      safelist: ["oder-0", "order-1", "order-2", "order-3", "order-4"],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
